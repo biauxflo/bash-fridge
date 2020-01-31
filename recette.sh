1ce91d0d921815cf6c8fc68e29ff03dea442046f
Amine Saidi <saidiamino20@gmail.com>
2020-01-31 15:46:10 +0100

    Ajout de recherche recette

--- recette.sh
+++ recette.sh
@@ -6,7 +6,7 @@ function chercher_recette {
         cut -d/ -f1 t1.txt > t2.txt
         tr ' ' '+' < t2.txt > t3.txt
         tr '\n' '+' < t3.txt > t4.txt
-        lien=$(echo "https://cuisine-libre.fr/?page=recherche&recherc$
+        lien=$(echo "https://cuisine-libre.fr/?page=recherche&recherche=")
         t5=`cat t4.txt`
         URL=$(echo $lien$t5)
         firefox $URL&
