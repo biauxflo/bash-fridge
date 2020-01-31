e93287ece5ebb81493737bfeabe70ebde5e84013
Florestan Biaux <florestan.biaux@etu.utc.fr>
2020-01-31 17:12:48 +0100

    Delete ajouter.sh

--- ajouter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function ajouter {
-        echo "Saisir les infos de votre produit (Nom/Quantite/Categorie/Date -AAAAMMJJ-) : "
-        read -r chaine
-        echo "$chaine" >> DATA.txt
-}
-
-ajouter
